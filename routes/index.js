2b51107e2225f337d5883a3b8f02c8a1bbe3093a
luis28ng <lcngtang@gmial.com>
2023-11-10 19:08:54 -0500

    structure setup

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+// import attendeesRoutes from './attendees.js';
+// import eventsRoutes from './events.js';
+
+// const constructorMethod = (app) => {
+//   app.use('/attendees', attendeesRoutes);
+//   app.use('/events', eventsRoutes);
+
+//   app.use('*', (req, res) => {
+//     res.status(404).json({error: 'Route Not found'});
+//   });
+// };
+
+// export default constructorMethod;
