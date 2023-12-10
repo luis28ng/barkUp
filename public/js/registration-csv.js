8d75f750d0a7fd00e99f66b6c2b5a1145dfc9255
rezkdavid <rezkdave@gmail.com>
2023-12-09 20:11:36 -0500

    Created CSV and Edited HBs
    Created the Login and Registration CSV and edited their rrespective handlebars

--- public/js/registration-csv.js
+++ public/js/registration-csv.js
@@ -1,6 +1,13 @@
 (function () {
   // Validation methods
   const validationMethods = {
+    validateName(name) {
+      const hasLettersOnly = /^[a-zA-Z]+$/.test(name);
+      if (!name) throw new Error("Name input is required");
+      if (!hasLettersOnly) throw new Error("Name ");
+      if (name.length < 2 || name.length > 25)
+        throw new Error("Name must be between 2 and 25 characters long.");
+    },
     validateEmail(email) {
       const emailRegex =
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -24,35 +31,52 @@
         throw new Error("Password must have at least one special character.");
       return true;
     },
+    confirmPassword(password, confirmPassword) {
+      if (password !== confirmPassword)
+        throw new Error("Passwords do not match");
+      return true;
+    },
   };
 
-  const loginForm = document.getElementById("login-form");
+  const registrationForm = document.getElementById("registration-form");
 
-  if (loginForm) {
+  if (registrationForm) {
+    const firstNameInput = document.getElementById("firstNameInput");
+    const lastNameInput = document.getElementById("lastNameInput");
     const emailInput = document.getElementById("emailAddressInput");
     const passwordInput = document.getElementById("passwordInput");
+    const confirmPasswordInput = document.getElementById(
+      "confirmPasswordInput"
+    );
 
-    // Create a new error container to hold and show errors
+    // Error container to hold and show errors
+
     errorContainer = document.createElement("div");
     errorContainer.classList.add("errors", "hidden");
-    loginForm.insertAdjacentElement("beforebegin", errorContainer);
+    registrationForm.insertAdjacentElement("beforebegin", errorContainer);
 
-    loginForm.addEventListener("submit", (event) => {
+    registrationForm.addEventListener("submit", (event) => {
       event.preventDefault();
 
-      // Hide error container by default
+      // Hide and clear error container by default
       errorContainer.classList.add("hidden");
-      errorContainer.innerHTML = ""; // Clear previous errors
+      errorContainer.innerHTML = "";
 
       try {
         // Validate input values
+        validationMethods.validateName(firstNameInput.value);
+        validationMethods.validateName(lastNameInput.value);
         validationMethods.validateEmail(emailInput.value);
         validationMethods.validatePassword(passwordInput.value);
+        validationMethods.confirmPassword(
+          passwordInput.value,
+          confirmPasswordInput.value
+        );
 
         // If validation passes, submit the form
-        loginForm.submit();
+        registrationForm.submit();
       } catch (e) {
-        // Show error message
+        // Show error messages
         const errorMessage = document.createElement("p");
         errorMessage.textContent = e;
         errorContainer.appendChild(errorMessage);
