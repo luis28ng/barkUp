3eaa30e6ae14b2bc700451c418f4075e775e9f4c
luis28ng <lcngtang@gmial.com>
2023-12-13 17:55:43 -0500

    delete reviews

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,6 +1,7 @@
 import { Router, json } from 'express';
 const router = Router();
 import reviews from '../data/reviews.js';
+import {checkId} from '../helpers.js';
 
 
 router.route('/').get(async (req, res) => {
@@ -9,18 +10,33 @@ router.route('/').get(async (req, res) => {
   });
 
 router.route('/:id').get(async (req, res) => {
-    const reviewId = req.params.id
+    let reviewId = req.params.id
     try {
         const review = await reviews.getReviewById(reviewId)
         const reviewTitle = review.reviewTitle;
         const reviewDescription = review.reviewDescription;
         const rating = review.rating;
 
-        res.render('review', { reviewTitle, reviewDescription, rating })
+        res.render('review', { reviewTitle, reviewDescription, rating, reviewId })
     } catch (e) {
-        res.status(404).json(`No review found with ID: ${reviewId}`);
+        res.redirect('/')
     };
     
+  })
+  .post(async (req,res) => {
+    let  reviewId = req.params.id
+    try {
+      reviewId = checkId(reviewId, 'Review Id')
+    } catch (e) {
+      return res.status(404).render('review', { deletionSuccess: false });
+    };
+
+    try {
+      const deleteReview = await reviews.deleteReview(reviewId);
+      return res.status(200).render('review', { deletionSuccess: true });
+    } catch (e) {
+      return res.status(404).render('review', { deletionSuccess: false });
+    }
   })
 
 
