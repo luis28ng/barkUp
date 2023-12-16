c7df3a531bfbccf9a821c52bda739b763faee2dc
jrailey13 <148562710+jrailey13@users.noreply.github.com>
2023-12-16 13:38:11 -0500

    Adding pet validation to helpers.js

--- helpers.js
+++ helpers.js
@@ -376,3 +376,15 @@ export const checkId = (id, varName) => {
   if (!ObjectId.isValid(id)) throw `Error: ${varName} invalid object ID`;
   return id;
 }
+
+export const validPet = (petName, petType, petBreed) => {
+  petName = petName.trim();
+  petType = petType.trim();
+  petBreed = petBreed.trim();
+
+  if (!petName || !petType || !petBreed) throw "All pet information must be supplied";
+
+  if (typeof petName !== 'string') throw "Pet name must be a string";
+  if (typeof petType !== 'string') throw "Pet type must be a string";
+  if (typeof petBreed !== 'string') throw "Pet breed must be a string";
+}
