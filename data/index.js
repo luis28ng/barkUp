ed1c951ca0d735695ccf8fd2952f680878baa549
jrailey13 <148562710+jrailey13@users.noreply.github.com>
2023-12-16 13:37:20 -0500

    Adding pet info to both index.js files

--- data/index.js
+++ data/index.js
@@ -2,9 +2,11 @@ import parkFunctions from "./parks.js"
 import petStoresFunctions from "./petStores.js"
 import reviewsFunctions from "./reviews.js"
 import userFunctions from "./users.js"
+import petFunctions from "./pets.js"
 
 
 export const parksData = parkFunctions;
 export const petStoresData = petStoresFunctions;
 export const reviewsData = reviewsFunctions;
 export const usersData = userFunctions;
+export const petData = petFunctions;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import allRoutes from "./routes.js";
 import reviewRoutes from "./reviewRoutes.js";
+import petRoutes from "./petRoutes.js";
 import { static as staticDir } from "express";
 const constructorMethod = (app) => {
   app.use("/", allRoutes);
   app.use("/review", reviewRoutes);
+  app.use("/pets", petRoutes);
   app.use("/public", staticDir("public"));
 
   app.use("*", (req, res) => {
